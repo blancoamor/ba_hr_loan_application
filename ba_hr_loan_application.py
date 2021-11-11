81ca793076d0f106c4318fcc315b4d34e7a4af72
filoquin <filoquin@gmail.com>
2021-11-11 19:44:56 -0300

    [FIX] Actualizo montos

--- ba_hr_loan_application.py
+++ ba_hr_loan_application.py
@@ -19,18 +19,17 @@
 #
 ##############################################################################
 
-from datetime import datetime , timedelta , date
+from datetime import datetime, timedelta, date
 from dateutil.relativedelta import relativedelta
 
-from openerp import models, fields, api 
+from openerp import models, fields, api
 from openerp import tools
 from openerp.exceptions import except_orm, Warning, RedirectWarning
 from openerp.tools.translate import _
 import re
 import logging
 
-_logger = logging.getLogger(__name__) 
-
+_logger = logging.getLogger(__name__)
 
 
 class ba_hr_loan_application(models.Model):
@@ -42,92 +41,94 @@ class ba_hr_loan_application(models.Model):
 
     @api.one
     def _compute_info(self):
-        self.loan_info = self.env['ir.config_parameter'].search([('key','=','loan_info')])['value']
-    
+        self.loan_info = self.env['ir.config_parameter'].search(
+            [('key', '=', 'loan_info')])['value']
+
     def _default_info(self):
-        return  self.env['ir.config_parameter'].search([('key','=','loan_info')])['value']
+        return self.env['ir.config_parameter'].search([('key', '=', 'loan_info')])['value']
 
+    state = fields.Selection([('request', 'Solicitado'),
+                              ('gestion', 'En Gestion'), ('deny', 'Denegado'), ('apply', 'Aprobado'), ('close', 'Finalizado')],
+                             track_visibility='onchange', default='request')
 
-    state = fields.Selection([('request','Solicitado'),
-                            ('gestion','En Gestion'),('deny','Denegado'),('apply','Aprobado'),('close','Finalizado')],
-                            track_visibility='onchange' , default = 'request' )
+    user_id = fields.Many2one('res.users', required=True)
 
-    user_id = fields.Many2one('res.users', required=True) 
+    loan_info = fields.Html(
+        'info', compute="_compute_info", default=_default_info)
 
-    loan_info = fields.Html('info' ,compute="_compute_info",default=_default_info ) 
+    name = fields.Many2one('hr.employee', compute="_compute_employee")
 
+    date = fields.Date('Fecha', default=date.today(), required=True)
+    antiquity = fields.Char(
+        'antigüedad', compute="_compute_antiquity", required=True)
 
-
-    name = fields.Many2one('hr.employee' ,compute="_compute_employee") 
-
-    date = fields.Date('Fecha', default=date.today() , required=True) 
-    antiquity = fields.Char('antigüedad' ,compute="_compute_antiquity" , required=True) 
-
-    causa = fields.Selection([('Salud','Salud'),('Financiero','Financiero'),('Alquiler','Alquiler')],
-                            string="Motivo de la Solicitud", required=True)
+    causa = fields.Selection([('Salud', 'Salud'), ('Financiero', 'Financiero'), ('Alquiler', 'Alquiler')],
+                             string="Motivo de la Solicitud", required=True)
     reason = fields.Text('Razon de la Solicitud', required=True)
-    emp_sanctions=fields.Boolean('¿Has tenido sanciones los últimos 3 Meses?') 
-    emp_assistance=fields.Boolean('¿Has tenido el 90% de presentismo los últimos 3 Meses?') 
+    emp_sanctions = fields.Boolean(
+        '¿Has tenido sanciones los últimos 3 Meses?')
+    emp_assistance = fields.Boolean(
+        '¿Has tenido el 90% de presentismo los últimos 3 Meses?')
 
-    emp_credit = fields.Boolean('¿Ténes descuentos por crédito Magenta?') 
-    emp_seizure = fields.Boolean('¿Ténes descuentos por embargo comercial?') 
+    emp_credit = fields.Boolean('¿Ténes descuentos por crédito Magenta?')
+    emp_seizure = fields.Boolean('¿Ténes descuentos por embargo comercial?')
 
-    amount = fields.Selection([('5000','5000'),('3000','3000')],
-                            string="Monto solicitado", required=True)
-    
-    fees = fields.Selection([('1','1'),('2','2'),('3','3'),('4','4'),('5','5')],
+    amount = fields.Selection([('1000', '1000'), ('5000', '5000'), ('3000', '3000')],
+                              string="Monto solicitado", required=True)
+
+    fees = fields.Selection([('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')],
                             string="Candidad de cuotas")
-    
 
-    magenta_credit = fields.Many2one('magenta.ayuda','Credito') 
+    magenta_credit = fields.Many2one('magenta.ayuda', 'Credito')
 
-    capital = fields.Float("Capital", related='magenta_credit.capital',store=True,readonly=True)
-    total_paga = fields.Float('Total Pagado',related='magenta_credit.total_paga',store=True,readonly=True)
+    capital = fields.Float(
+        "Capital", related='magenta_credit.capital', store=True, readonly=True)
+    total_paga = fields.Float(
+        'Total Pagado', related='magenta_credit.total_paga', store=True, readonly=True)
     #magenta_ayuda_ctas_ids = fields.One2many('magenta.ayuda.ctas','magenta_ayuda_id',)
-
-
-
 
     @api.multi
     @api.depends('user_id')
     def _compute_employee(self):
-        for loan in self: 
-            if loan.user_id :
-                employee_id = self.env['hr.employee'].search([('user_id','=',loan.user_id.id)])
+        for loan in self:
+            if loan.user_id:
+                employee_id = self.env['hr.employee'].search(
+                    [('user_id', '=', loan.user_id.id)])
                 if employee_id:
                     loan.name = employee_id[0]
 
-
-    @api.depends('date','name')
+    @api.depends('date', 'name')
     def _compute_antiquity(self):
-        diff = relativedelta(fields.Date.from_string(self.date) , fields.Date.from_string(self.name.admision_date)).years
+        diff = relativedelta(fields.Date.from_string(
+            self.date), fields.Date.from_string(self.name.admision_date)).years
         self.antiquity = "%d años" % diff
 
-
     @api.model
-    def create(self,vals) :
+    def create(self, vals):
 
         vals['message_follower_ids'] = []
 
-        blancoamor_credito = self.env['ir.model.data'].get_object('magenta', 'blancoamor_credito')
-        for user in blancoamor_credito.users :
+        blancoamor_credito = self.env['ir.model.data'].get_object(
+            'magenta', 'blancoamor_credito')
+        for user in blancoamor_credito.users:
             vals['message_follower_ids'].append(user.partner_id.id)
-        blancoamor_rh = self.env['ir.model.data'].get_object('ba_conf', 'blancoamor_rh')
-        for user in blancoamor_rh.users :
+        blancoamor_rh = self.env['ir.model.data'].get_object(
+            'ba_conf', 'blancoamor_rh')
+        for user in blancoamor_rh.users:
             vals['message_follower_ids'].append(user.partner_id.id)
 
         #blancoamor_credito = self.env['ir.model.data'].get_object( 'ba_conf', 'blancoamor_credito')
-        rec = super(ba_hr_loan_application, self).create(vals)   
-        return rec        
+        rec = super(ba_hr_loan_application, self).create(vals)
+        return rec
 
     @api.one
     def deny(self):
-        self.write({'state':'deny'})
+        self.write({'state': 'deny'})
 
     @api.one
     def apply(self):
-        self.write({'state':'apply'})
+        self.write({'state': 'apply'})
 
     @api.one
     def gestion(self):
-        self.write({'state':'gestion'})
+        self.write({'state': 'gestion'})
